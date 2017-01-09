1f377f455d951e8d90dffc753ab1998b0e212b28
Federica Polvara <Polly@MBP-di-Federica.fritz.box>
2017-01-09 18:25:27 +0100

    prova

--- sketch.js/lib/sketch.js
+++ sketch.js/lib/sketch.js
@@ -170,7 +170,7 @@ var __slice = Array.prototype.slice;
     draw: function(action) {
       var oldcomposite;
       oldcomposite = this.context.globalCompositeOperation;
-      this.context.globalCompositeOperation = "copy";
+      this.context.globalCompositeOperation = "destination-out";
       action.color = "rgba(0,0,0,0)";
       $.sketch.tools.marker.draw.call(this, action);
       return this.context.globalCompositeOperation = oldcomposite;
